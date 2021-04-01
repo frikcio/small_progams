548d0498e17f169fe6f4bab0dd31da9ba212afdb
frikcio <frikcio67@gmail.com>
2021-04-01 14:19:28 +0300

    a little optimization

--- Calculation.py
+++ Calculation.py
@@ -1,90 +1,89 @@
 from colorama import init
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 init()
 
+
 def my_error():
-    print(Fore.RED + "\nWRONG COMAND!\n\n")
+    print(Fore.RED + "\nWRONG COMMAND!\n\n")
 
 
 def num_error():
     print(Fore.RED + "\nERROR!\n")
-    print("Pleace input a number\n\n" + Fore.GREEN)
+    print("Please input a number\n\n" + Fore.GREEN)
 
 
 def check():
     while True:
-        num2 = input(Fore.BLUE + "\n2-nd number:  " + Fore.GREEN)
-        if num2.isdigit():
-            break
+        number2 = input(Fore.BLUE + "\n2-nd number:  " + Fore.GREEN)
+        if number2.isdigit():
+            return number2
         else:
             num_error()
-    return num2
+    
+
+def plus(number1):
+    number2 = check()
+    return float(number1) + float(number2)
 
 
-def plus(num1):
-    num2 = check()
-    return float(num1) + float(num2)
+def minus(number1):
+    number2 = check()
+    return float(number1) - float(number2)
 
 
-def minus(num1):
-    num2 = check()
-    return float(num1) - float(num2)
+def times(number1):
+    number2 = check()
+    return float(number1) * float(number2)
 
 
-def times(num1):
-    num2 = check()
-    return float(num1) * float(num2)
-
-
-def divide(num1):
+def divide(number1):
     while True:
-        num2 = input(Fore.BLUE + "\n2-nd number:  " + Fore.GREEN)
-        if num2.isdigit():
+        number2 = input(Fore.BLUE + "\n2-nd number:  " + Fore.GREEN)
+        if number2.isdigit():
             try:
-                result = float(num1) / float(num2)
-                break
+                results = float(number1) / float(number2)
+                return results
             except ZeroDivisionError:
                 print(Fore.RED + "\nCANNOT DIVIDED BY 0!!\n")
                 continue
         else:
             num_error()
             continue
-    return float(num1) / float(num2)
 
 
-operations = {
+operations_list = {
     "+": plus,
     "-": minus,
     "*": times,
     "/": divide}
 
 
-def run_operation(num1, operations, operation):
-    return operations[operation](num1)
+def run_operation(number1, operations_list, operation):
+    return operations_list[operation](number1)
 
 
 # Instruction block BEGIN
-def help():
-	print(Fore.MAGENTA + '\n\nINSTRUCTION:\n')
-	print(Fore.YELLOW + 'Input a number and press:')
-	print(Fore.GREEN + ' "+" ' + Fore.YELLOW + ' if you want add numbers;')
-	print(Fore.GREEN + ' "-" ' + Fore.YELLOW + ' if you want subtract numbers;')
-	print(Fore.GREEN + ' "*" ' + Fore.YELLOW + ' if you want multiply numbers;')
-	print(Fore.GREEN + ' "/" ' + Fore.YELLOW + ' if you want divide the numbers;')
-	print('Enter comand ' + Fore.GREEN + ' "help"' + Fore.YELLOW + ', if you want to see Instruction one more time;')
-	print('Enter comand ' + Fore.GREEN + ' "exit"' + Fore.YELLOW + ', if you want to close this programm;' + Fore.BLUE)
+def help_instruction():
+    print(Fore.MAGENTA + '\n\nINSTRUCTION:\n',
+          Fore.YELLOW + 'Input a number and press:\n',
+          Fore.GREEN + ' "+" ' + Fore.YELLOW + ' if you want add numbers;\n',
+          Fore.GREEN + ' "-" ' + Fore.YELLOW + ' if you want subtract numbers;\n',
+          Fore.GREEN + ' "*" ' + Fore.YELLOW + ' if you want multiply numbers;\n',
+          Fore.GREEN + ' "/" ' + Fore.YELLOW + ' if you want divide the numbers;\n',
+          'Enter command ' + Fore.GREEN + ' "help"' + Fore.YELLOW + ', if you want to see Instruction one more time;\n',
+          'And ' + Fore.GREEN + ' "exit"' + Fore.YELLOW + ', if you want to close this program;\n' + Fore.BLUE)
 # Instruction block ENDING
 
-# Calculation module
-help()
+
+help_instruction()  # Calculation module
 while True:
     print(Fore.BLUE + "\nStart the calculation\n")
-    num1 = input("1-st number:  " + Fore.GREEN)
+    number1 = input("1-st number:  " + Fore.GREEN)
 
     # EXIT module
-    if num1 == "exit":
-        print(Fore.YELLOW + "Are you want to close this programm? " + Fore.BLUE + "(1 - YES/2 - NO)" + Fore.GREEN)
+    if number1 == "exit":
+        print(Fore.YELLOW + "Are you want to close this program? " + Fore.BLUE + "(1 - YES/2 - NO)" + Fore.GREEN)
         answer = input(":  ")
         if answer == "1":
             print(Fore.YELLOW + "\n\n calculation is over!")
@@ -95,16 +94,16 @@ while True:
             my_error()
             continue
 
-    elif num1 == "help":
-    	help()
-    	continue
+    elif number1 == "help":
+        help_instruction()
+        continue
 
-    elif num1.isdigit():
+    elif number1.isdigit():
         # OPERATION module
         print(Fore.GREEN)
         operation = input(Fore.BLUE + "operation:  " + Fore.GREEN)
-        if operation in operations:
-            result = run_operation(num1, operations, operation)
+        if operation in operations_list:
+            result = run_operation(number1, operations_list, operation)
             print(Fore.BLUE + "\nResult: " + Fore.GREEN + str(result) + "\n\n")
         else:
             my_error()
